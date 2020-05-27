df90df1052ac69ee52ad84d757025170fd0608de
Hachi-Dev <dt0953825502@gmail.com>
2020-05-27 11:32:12 +0800

    added java data type

--- src/main/java/com/nexsys/Hello.java
+++ src/main/java/com/nexsys/Hello.java
@@ -5,5 +5,8 @@ public class Hello {
 //        System.out.println("JAVA Hello");
         Person p = new Person();
         p.hello();
+        int age = 19;
+        float h = 19.88f;
+        char c = 't';
     }
 }
